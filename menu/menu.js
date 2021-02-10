c303c5602409ae5faf404b0b2c88ede76e6cdd30
Guif98 <guif61998@gmail.com>
2021-02-10 13:54:54 -0300

    modified menu.js

--- menu/menu.js
+++ menu/menu.js
@@ -10,7 +10,7 @@ const menu = [
     {
         id:2,
         title: 'Fried steak with french fries',
-        category: 'Lunch',
+        category: 'lunch',
         price: 12.00,
         img: "images/friedSteak.jpg",
         desc: 'Fried Steak is the best thing ever, even more with french fries.'
@@ -18,7 +18,7 @@ const menu = [
     {
         id:3,
         title: 'Cheesecake',
-        category: 'Breakfast',
+        category: 'breakfast',
         price: 4,
         img: 'images/cheesecake.jpg',
         desc: 'Cheesecakes are the best thing to be eaten by the morning'
@@ -27,11 +27,41 @@ const menu = [
 
 const sectionCenter = document.querySelector(".section-center");
 const menuBtn = document.querySelectorAll(".menu-btn");
-console.log(menuBtn);
+
+//load items
+window.addEventListener("DOMContentLoaded", function() {
+    displayMenuItems(menu);
+
+    const categories = menu.map(function (item) {
+        if (!values.includes(item.category)) {
+            values.push(item.category);
+        } 
+        return values;
+    })
+});
+
+//filter items
+menuBtn.forEach(function (btn) {
+    btn.addEventListener('click', function(e){
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function(menuItem){
+            if (menuItem.category === category) {
+                return menuItem;
+            }
+        });
+        if (category === 'All') {
+            displayMenuItems(menu);
+        }
+        else {
+            displayMenuItems(menuCategory);
+        }
+    })
+});
 
 
-window.addEventListener('DOMContentLoaded', function(){
-    let displayMenu = menu.map(function (item){
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function (item){
         return `<article class="menu-item mb-5">
                 <img class="w-50 shadow photo" style="border: 4px solid rgb(156, 115, 0); width: 50%"  src="${item.img}" alt="${item.title}">
                 <div class="item-info">
@@ -47,4 +77,4 @@ window.addEventListener('DOMContentLoaded', function(){
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu
-});
+}
